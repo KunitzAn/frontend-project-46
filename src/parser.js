86bfcd0e78efc17dc779919f3f6193feb9d1922f
root <root@MSI>
2023-01-22 14:51:11 +0300

    update src/parse.js

--- src/parser.js
+++ src/parser.js
@@ -6,10 +6,8 @@ const diff = (data1, data2) => {
     const keys2 = Object.keys(data2);
     const keys = _.union(keys1, keys2);
     keys.sort();
-
    const result = [];
    result.push('{');
-
    keys.map((key) => {
     // если равны ключи и значения
     if (_.has(data1, key) && _.has(data2, key)) {
@@ -28,13 +26,11 @@ const diff = (data1, data2) => {
     if (_.has(data1, key) && !_.has(data2, key)) {
       result.push(`  - ${key}: ${data1[key]}`);
     }
-
     // если ключ есть во втором файле но отсутствует в первом
     if (!_.has(data1, key) && _.has(data2, key)) {
       result.push(`  + ${key}: ${data2[key]}`);
     }
   });
-
   result.push('}');
   return result.join('\n');
 }
