644fce3ef7acf0cd7702e8c4a4478eabe92740d6
root <root@MSI>
2023-01-22 14:21:54 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-
+import parser from '../src/parser.js';
 
 program 
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
     .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format')
+
+    .action((filepath1, filepath2) => {
+        console.log(parser(filepath1, filepath2));
+    })
 
 program.parse(process.arvs);
 
