73d480c32f0eb634a97251d69039db6604149062
root <root@MSI>
2023-01-26 19:33:27 +0300

    step 5: add yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,19 +6,21 @@ import parser from '../src/parser.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//console.log(__filename);
-//console.log(__dirname);
-
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-//console.log(`getFixturedPath:${getFixturePath('file1.json')}`); 
+const getPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 
 test('file json', () => {
-    const filename1 = getFixturePath('file1.json');
-    const filename2 = getFixturePath('file2.json');
-    const resultname = getFixturePath('file_result.txt');
+    const filename1 = getPath('file1.json');
+    const filename2 = getPath('file2.json');
+    const resultname = getPath('file_result.txt');
     const result = readFileSync(resultname, 'utf8');
     expect(parser(filename1, filename2)).toBe(result);
 });
 
+test('file yml', () => {
+    const filename1 = getPath('file1.yml');
+    const filename2 = getPath('file2.yml');
+    const resultname = getPath('file_result.txt');
+    const result = readFileSync(resultname, 'utf8');
+    expect(parser(filename1, filename2)).toBe(result);
+});
--- src/parser.js
+++ src/parser.js
@@ -1,47 +1,20 @@
 import { readFileSync } from 'fs';
-import _ from 'lodash';
+import path from 'path';
+import diff from './buildDiffs.js';
+import parse from './parsers.js';
 
-const diff = (data1, data2) => {
-    const keys1 = Object.keys(data1);
-    const keys2 = Object.keys(data2);
-    const keys = _.union(keys1, keys2);
-    keys.sort();
-   const result = [];
-   result.push('{');
-   keys.map((key) => {
-    // если равны ключи и значения
-    if (_.has(data1, key) && _.has(data2, key)) {
-      if (data1[key] === data2[key]) {
-        result.push(`    ${key}: ${data1[key]}`);
-      }
-    }
-    // если ключи равны, но значения разные
-    if (_.has(data1, key) && _.has(data2, key)) {
-      if (data1[key] !== data2[key]) {
-        result.push(`  - ${key}: ${data1[key]}`);
-        result.push(`  + ${key}: ${data2[key]}`);
-      }
-    }
-    // если ключ есть в первом файле но отсутствует во втором
-    if (_.has(data1, key) && !_.has(data2, key)) {
-      result.push(`  - ${key}: ${data1[key]}`);
-    }
-    // если ключ есть во втором файле но отсутствует в первом
-    if (!_.has(data1, key) && _.has(data2, key)) {
-      result.push(`  + ${key}: ${data2[key]}`);
-    }
-  });
-  result.push('}');
-  return result.join('\n');
-}
+const getPath = (filename) => path.resolve(process.cwd(), filename);
+const getFileFormat = (filename) => filename.split('.')[1];
+const readFile = (filepath) => readFileSync(filepath, 'utf8');
+
 
 
 const parser = (filepath1, filepath2) => {
-    const data1 = readFileSync(filepath1, 'utf-8');
-    const data2 = readFileSync(filepath2, 'utf-8');
+    const data1 = getPath(filepath1);
+    const data2 = getPath(filepath2);
   
-    const dataParse1 = JSON.parse(data1);
-    const dataParse2 = JSON.parse(data2);
+    const dataParse1 = parse(readFile(data1), getFileFormat(filepath1));
+    const dataParse2 = parse(readFile(data2), getFileFormat(filepath2));
   
     return diff(dataParse1, dataParse2);
 };
