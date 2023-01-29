c74340d9c16d6135c2254420aaa52d9f2df35756
root <root@MSI>
2023-01-29 17:07:38 +0300

    update -4

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -7,41 +7,20 @@ const findDiff = (data1, data2) => {
 
   const result = sortedKeys.flatMap((key) => {
     if (!_.has(data2, key)) { // ключ есть в первом файле, но отсутствует во втором
-      return {
-        key,
-        value: data1[key],
-        state: 'removed',
-      };
-    } 
+      return {key, value: data1[key], state: 'removed'};
+    }
     if (!_.has(data1, key)) { // ключ есть во втором файле, но отсутствует в первом
-      return {
-        key,
-        value: data2[key],
-        state: 'added',
-      };
-    } 
+      return {key, value: data2[key], state: 'added'};
+    }
     if (data1[key] === data2[key]) { // равны ключи и значения
-      return {
-        key,
-        value: data1[key],
-        state: 'unchanged',
-      };
-    } 
+      return {key, value: data1[key], state: 'unchanged'};
+    }
     if (_.isObject(data1[key]) && _.isObject(data2[key])) { // есть вложенные объекты
-      return {
-        key,
-        value: findDiff(data1[key], data2[key]),
-        state: 'nested',
-      };
-    } 
+      return {key, value: findDiff(data1[key], data2[key]), state: 'nested'};
+    }
     // ключи одинаковые, но значения разные
-    return {
-      key,
-      value: { oldValue: data1[key], newValue: data2[key] },
-      state: 'updated',
-    };
+    return {key, value: { oldValue: data1[key], newValue: data2[key] }, state: 'updated'};
   });
-
   return result;
 };
 /*
