8bf916aa995138dadd7c36d69fb3628bc249436a
root <root@MSI>
2023-01-29 16:56:00 +0300

    update

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -6,24 +6,37 @@ const findDiff = (data1, data2) => {
   const sortedKeys = _.sortBy(_.union(keys1, keys2));
 
   const result = sortedKeys.flatMap((key) => {
-    const diff = {key};
     if (!_.has(data2, key)) { // ключ есть в первом файле, но отсутствует во втором
-      diff.value = data1[key];
-      diff.state = 'removed'
+      return { 
+        key, 
+        value: data1[key], 
+        state: 'removed',
+      };
     } else if (!_.has(data1, key)) { // ключ есть во втором файле, но отсутствует в первом
-      diff.value = data2[key];
-      diff.state = 'added';
+      return {
+        key,
+        value: data2[key],
+        state: 'added',
+      };
     } else if (data1[key] === data2[key]) { // равны ключи и значения
-      diff.value = data1[key];
-      diff.state = 'unchanged';
+      return {
+        key,
+        value: data1[key],
+        state: 'unchanged',
+      };
     } else if (_.isObject(data1[key]) && _.isObject(data2[key])) { // есть вложенные объекты
-      diff.value = findDiff(data1[key], data2[key]);
-      diff.state = 'nested';
+      return {
+        key,
+        value: findDiff(data1[key], data2[key]),
+        state: 'nested',
+      };
     } else { // ключи одинаковые, но значения разные
-      diff.value = { oldValue: data1[key], newValue: data2[key] };
-      diff.state = 'updated';
+      return {
+        key,
+        value: { oldValue: data1[key], newValue: data2[key] },
+        state: 'updated',
+      };
     }
-    return diff;
   });
 
   return result;
